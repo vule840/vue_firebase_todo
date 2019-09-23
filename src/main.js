d1f0cdf400c964b4c7bf3eff14e2f305d585ef32
vule840 <vule840@gmail.com>
2019-09-23 23:21:13 +0200

    1

--- src/main.js
+++ src/main.js
@@ -2,8 +2,28 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import firebase from 'firebase'
 
 Vue.config.productionTip = false
+
+
+const config = {
+   apiKey: "AIzaSyAfllZlZx9bF6ubF8QHgXJxVrRF6WJL09Y",
+    authDomain: "testing-39571.firebaseapp.com",
+    databaseURL: "https://testing-39571.firebaseio.com",
+    projectId: "testing-39571",
+    storageBucket: "testing-39571.appspot.com",
+    messagingSenderId: "1007988683941",
+    appId: "1:1007988683941:web:110ad0273da0e26714094c"
+}
+
+firebase.initializeApp(config)
+
+export const db = firebase.firestore()
+
+export const testExport = "ovo je ribas"
+
+// console.log(db)
 
 new Vue({
   router,
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,152 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="title">What do I need to do today?</div>
+    <input v-model="myTodo" type="" name=""><button @click="addToDo">Add</button>
+    <!-- div>{{myTodo}}</div> -->
+    <div v-if="this.errors !== ''">{{errors}}</div>
+
+   <!--  <hr>
+
+    <button @click="prazno">Pitaj me dali je prazno</button> 
+
+    <input type="text" v-model="data" name="">
+	<div v-if="this.bugovi !== ''">{{bugovi}}</div> -->
+	<div v-if="this.$store.getters.getItems && this.$store.getters.getItems.length > 0">
+      <div class="title">Today, you've go to do...</div>
+ 
+     <div v-for="item in this.$store.getters.getItems" :key="item.id">
+       {{ item.title }}<br /><br /><small style="text-decoration:underline;" @click="deleteItem(item.id)">Delete</small>
+       <hr />
+     </div>
+   </div>
+</div>
+		
+
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+//https://www.codewall.co.uk/how-to-create-a-real-time-to-do-list-app-with-vue-vuex-firebase-tutorial/
+import { testExport } from '@/main'
+import { db } from '@/main'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    // HelloWorld
+  },
+  data(){
+  	return{
+  		myTodo: "",
+  		errors: "",
+  		data: '',
+  		bugovi: ''
+
+  	}
+  },
+  beforeCreate(){
+
+  		// this.$store.dispatch('setitems')
+  		// console.log(this.$store.getters.getItems)
+  },
+  methods:{
+  	addToDo(){
+  		console.log('radi li ' + this.myTodo)
+  		console.log(testExport)
+  		console.log(this.$store.state.items)
+  		this.errors = ""
+  		if(this.myTodo !== ''){
+  			db.collection('items').add({
+  				title: this.myTodo,
+  				created_at: Date.now()	
+
+  			}).then((response) => {
+  				if(response){
+  					this.myTodo = ''
+  				}	
+
+  			} ).catch((erorr) => {
+  				this.errors = errors
+  			})
+
+
+  			this.myTodo
+
+  		}
+
+  		else{
+  			this.errors = "nemre biti prazno, upiši text"
+  		}
+  		
+
+  	 }
+
+  	// ,prazno(){
+  	// 	//////////
+  	// 	this.bugovi = ''
+  	// 	if(this.data !== ''){
+  	// 		this.data
+  	// 	}else{
+  	// 		this.bugovi = "e sori ovo je prazno, upiši nešto"
+  	// 	}
+  	// }
   }
 }
 </script>
+<style>
+	* {
+  box-sizing:border-box;
+}
+
+body, html, #app {
+  background:#8ac8e5;
+}
+
+.home {
+  width:300px;
+  margin:auto;
+}
+
+#vue-logo {
+  width:100px;
+}
+
+input, button {
+  border:0;
+  width:100%;
+  margin:0 0 10px;
+  padding:7px;
+}
+
+input {
+  font-size:12px;
+}
+
+button {
+  background:#43b823;
+  border:0;
+  text-transform:uppercase;
+  color:#fff;
+  font-weight:700;
+  cursor:pointer;
+}
+
+.title {
+  font-size:14px;
+  font-weight:700;
+  padding:0 0 10px 0;
+  margin:0 0 10px 0;
+  border-bottom:1px solid #666;
+}
+
+#errors {
+  background:#a52222;
+  color:#fff;
+  padding:5px;
+}
+
+</style>
